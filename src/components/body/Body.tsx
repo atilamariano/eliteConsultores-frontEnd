9016efdddc6f6381639c926b9f8604f190627243
atilamariano <atilamariano@hotmail.com>
2023-06-30 11:32:24 -0300

    feat: implement acordeon to body

--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { GetAllCompanies } from '../../services/companyReq/getAllCompanies';
-import { Link } from 'react-router-dom';
-
-interface Company {
-    businessName: string;
-}
+import { ICompany } from '../../interfaces/ICompany';
 
 function Body() {
-    const [companies, setCompanies] = useState<string[]>([]);
+    const [companies, setCompanies] = useState<ICompany[]>([]);
+    const [expandedCompany, setExpandedCompany] = useState<string>('');
 
     useEffect(() => {
         fetchCompanies();
@@ -16,25 +13,44 @@ function Body() {
     async function fetchCompanies() {
         try {
             const response = await GetAllCompanies();
-            const businessNames = response.data.map((company: Company) => company.businessName);
-            setCompanies(businessNames);
+            setCompanies(response.data);
         } catch (error) {
             console.error('Error getting companies', error);
         }
+    }
+
+    function handleExpandCompany(companyId: string) {
+        setExpandedCompany(companyId === expandedCompany ? '' : companyId);
     }
 
     return (
         <div>
             <h1>Lista de Empresas</h1>
             <ul>
-                {companies.map((businessName, id) => (
-                    <li key={id}>
-                        <Link to='/profile'>{businessName}</Link>
+                {companies.map((company) => (
+                    <li key={company.id}>
+                        <button onClick={() => handleExpandCompany(company.id)}>
+                            {company.businessName}
+                        </button>
+                        {company.id === expandedCompany && (
+                            <div>
+                                <p>Código: {company.code}</p>
+                                <p>CNPJ: {company.cnpj}</p>
+                                <p>Contato: {company.contactPerson}</p>
+                                <p>Telefone de Contato: {company.contactPhone}</p>
+                                <p>Email de Contato: {company.contactEmail}</p>
+                                <p>
+                                    Data de Inclusão: {company.inclusionDate}
+                                </p>
+                                <p>Status: {company.status}</p>
+                                <p>Inscrição Municipal: {company.municipalRegistration}</p>
+                            </div>
+                        )}
                     </li>
                 ))}
             </ul>
         </div>
-    )
+    );
 }
 
-export default Body
+export default Body;
--- src/interfaces/ICompany.ts
+++ src/interfaces/ICompany.ts
@@ -1,16 +1,12 @@
-const EnumStatus = {
-  ACTIVE: "acitive",
-  INATIVE: "inative",
-};
-
 export interface ICompany {
+  id: string;
   code: string;
   cnpj: string;
   businessName: string;
   contactPerson: string;
   contactPhone: string;
   contactEmail: string;
-  inclusionDtae: Date;
-  status?: typeof EnumStatus;
+  inclusionDate?: string;
+  status?: string;
   municipalRegistration: string;
 }
